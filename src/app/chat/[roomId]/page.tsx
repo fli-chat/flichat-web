fa4d9ebc718a9e87c340776dcab4285cea36dac9
jutrong <leek71358@gmail.com>
2025-10-14 11:06:16 +0900

    Chore: parmas return Promise

--- src/app/chat/[roomId]/page.tsx
+++ src/app/chat/[roomId]/page.tsx
@@ -4,29 +4,27 @@ import { META_MAP } from "@/app/chat/[roomId]/metaMap";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export async function generateMetadata(
-  { params }: { params: { roomId: number } }
+  { params }: { params: Promise<{ roomId: number }> }
 ): Promise<Metadata> {
-  return META_MAP[params.roomId] ?? {
+  const { roomId } = await params;
+
+  return META_MAP[roomId] ?? {
     title: "FliChat 채팅방",
     description: "영화 팬 커뮤니티 실시간 채팅",
   };
 }
 
+export default async function Page({
+  params
+}: {
+  params: Promise<{ roomId: number }>
+}) {
+  const { roomId } = await params;
 
-
-export default async function Page({ params }: { params: { roomId: number } }) {
-  const { roomId } = params;
-
-
-  // ✅ 동일한 데이터 소스 재사용
   const meta = META_MAP[roomId] ?? {
     title: "FliChat 채팅방",
     description: "영화 팬 커뮤니티 실시간 채팅",
   };
-
-  // (선택) 방 존재여부/권한 체크(없으면 notFound())
-  // const room = await fetchRoomMeta(roomId);
-  // if (!room) notFound();
 
   return (
     <ChatRoomClient roomId={roomId} title={meta.title as string} />
